7ed514d87545749e25e03cf7fc60ef6316e3ed4e
FUM <53284113+fuhito516@users.noreply.github.com>
2021-05-27 18:32:36 +0900

    up

--- Assets/Miya/miya_scene/変更点メモ.cs
+++ Assets/Miya/miya_scene/変更点メモ.cs
@@ -6,8 +6,12 @@ public class 変更点メモ : MonoBehaviour
 {
     // 変更点
 
+    // アニメーションを合わせる
     // PlayerのScaleを0.8に変更
 
-    // Yojinoboriアニメーションのループを外してAply
+    // 挙動ズレ
+    // Yojinoboriアニメーションのループを外してAply(済)
     // InspectorのSecondClimbの時間を伸ばす
+    // AnimationのClimb→Waitの矢印内の遷移を0%に変更,つまり補間を無くす
+    // 状態をそのフレームでもどす。sc_state.Set_AnimationState(Player_State.e_PlayerAnimationState.WAITING);
 }
