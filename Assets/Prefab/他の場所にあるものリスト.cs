08911587c326b659d31284d599b6fce820f94111
FUM <53284113+fuhito516@users.noreply.github.com>
2021-05-30 14:55:30 +0900

    プレハブ

---
+++ Assets/Prefab/他の場所にあるものリスト.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 他の場所にあるものリスト : MonoBehaviour
+{
+    // UI
+    // harada>prehub>UIとmiyamoto_UI>clear>canbas
+}
