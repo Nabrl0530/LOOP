ae3927cbda147b800622d5e49149bae2fb9f1f6d
FUM <53284113+fuhito516@users.noreply.github.com>
2021-06-23 00:46:21 +0900

    sound 全体適用 （次：コントローラー入力→数値グリット→メニューへ導入

--- Assets/Miya/miya_scene/miya_test_UI.cs
+++ Assets/Miya/miya_scene/miya_test_UI.cs
@@ -7,28 +7,61 @@ public class miya_test_UI : MonoBehaviour
 {
 	public GameObject UI_window;
 
+	// テストサウンド
+	public GameObject TestBGM;
+	public GameObject TestSE;
+	AudioSource TestBGM_audio;
+	AudioSource TestSE_audio;
+	float FirstVolume_BGM;
+	float FirstVolume_SE;
+
 	// シリンダー
 	Slider slider_bgm;
 	Slider slider_se;
+
+	// ボリューム
+	static public float Magnification_BGM = 0.5f;
+	static public float Magnification_SE = 0.5f;
 
 	// デバッグ
 	bool active = false;
 
     // Start is called before the first frame update
     void Start()
-    {
+	{
+		// テストサウンド
+		if (TestBGM) TestBGM_audio = TestBGM.GetComponent<AudioSource>();
+		if (TestSE) TestSE_audio = TestSE.GetComponent<AudioSource>();
+		FirstVolume_BGM = TestBGM_audio.volume;
+		FirstVolume_SE = TestSE_audio.volume;
+
 		// シリンダー
 		GameObject back = this.transform.Find("Back").gameObject;
 		slider_bgm = back.transform.Find("Slider_BGM").GetComponent<Slider>();
 		slider_se = back.transform.Find("Slider_SE").GetComponent<Slider>();
 
+		// ボリューム
+		Magnification_BGM = 0.5f;
+		Magnification_SE = 0.5f;
+
 		// デバッグ
 		active = false;
 	}
-
-    // Update is called once per frame
-    void Update()
+	
+	// Update is called once per frame
+	void Update()
 	{
+		// ボリューム
+		Magnification_BGM = slider_bgm.value;
+		Magnification_SE = slider_se.value;
+
+		// テストプレイ
+		TestBGM_audio.volume = FirstVolume_BGM * Magnification_BGM;
+		TestSE_audio.volume = FirstVolume_SE * Magnification_SE;
+
+		// SEテストプレイ
+		if (Input.GetMouseButtonUp(0)) TestSE_audio.Play();
+
 		// デバッグ
 		{
 			if (Input.GetKeyUp(KeyCode.P))
@@ -39,12 +72,12 @@ public class miya_test_UI : MonoBehaviour
 			if (active)
 			{
 				Show_Window();
-				Debug.Log("Show");
+				//Debug.Log("Show");
 			}
 			else
 			{
 				Close_Window();
-				Debug.Log("Close");
+				//Debug.Log("Close");
 			}
 		}
 	}
--- Assets/Sound/miya_sound_control.cs
+++ Assets/Sound/miya_sound_control.cs
@@ -4,14 +4,36 @@ using UnityEngine;
 
 public class miya_sound_control : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	// どっち
+	public bool BGMtrueSEfalse = true;
+	
+	// ソース
+	AudioSource Sound;
 
-    // Update is called once per frame
-    void Update()
+	// ボリューム
+	float Volume;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		// どっち
+		BGMtrueSEfalse = true;
+
+		// ソース
+		Sound = this.GetComponent<AudioSource>();
+
+		// ボリューム
+		Volume = Sound.volume;
+		//Debug.Log(Volume);
+
+		// どっち
+		if ( BGMtrueSEfalse )	Sound.volume = Volume * miya_test_UI.Magnification_BGM;
+		else					Sound.volume = Volume * miya_test_UI.Magnification_SE;
+		//Debug.Log(miya_test_UI.Magnification_BGM);
+	}
+
+	// Update is called once per frame
+	void Update()
     {
         
     }
