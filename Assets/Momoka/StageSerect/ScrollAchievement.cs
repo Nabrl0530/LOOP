55aa9d443d7daf864a8e3b43879eb6cd8a8fe5e6
harada_yuuki <harada.yuuki.03@gmail.com>
2021-07-15 10:06:16 +0900

    スクロールスクリプトバグ修正コピペ、草生やすやつ導入

--- Assets/Momoka/StageSerect/ScrollAchievement.cs
+++ Assets/Momoka/StageSerect/ScrollAchievement.cs
@@ -21,30 +21,43 @@ public class ScrollAchievement : MonoBehaviour
         data = Data.Instance;
         string s;
 
+        s = JsonUtility.ToJson(data);
+        Debug.Log(s);
+
+        Debug.Log(data.DataNum);
+
+
         //ã‚©‚çŒŸõ
-        for (int i = data.DataNum-1; i >= 0; i--)
+        for (int i = data.DataNum - 1; i > 0; i--)
         {
             s = "stage" + (i + 1).ToString("00");
 
+            Debug.Log(i + ":" + data.DataNum);
 
-            if (data.GetStageStatus(i) == (int)Data.STAGE_STATUS.NONE)
-                transform.Find(s).Find("lock").gameObject.SetActive(true);
-            else if(data.GetStageStatus(i) == (int)Data.STAGE_STATUS.CLEAR)
+            if (data.GetStageStatus(i - 1) == (int)Data.STAGE_STATUS.CLEAR)
             {
                 //ƒNƒŠƒA‚İ‚Â‚¯‚½‚çŸ‚ğopen‚É‚·‚é(Ÿ‚ª‰ğ•ú‚³‚ê‚Ä‚È‚¢ê‡)
                 //‰º‚Ì‚â‚Â‚©‚¦‚é
                 //‚ ‚ÆŒ®ŠJ‚¯‚½‚Æ‚«‚Ì‰¹‚ğ‚Â‚¯‚é
-                s = "stage" + (i + 2).ToString("00");
-                if (data.GetStageStatus(i + 1) != (int)Data.STAGE_STATUS.NONE)
+                s = "stage" + (i + 1).ToString("00");
+                if (data.GetStageStatus(i) != (int)Data.STAGE_STATUS.NONE)
                     continue;
 
-                data.SetStageStatus(i + 1, Data.STAGE_STATUS.NEW);
+                data.SetStageStatus(i, Data.STAGE_STATUS.NEW);
                 GameObject obj = transform.Find(s).Find("lock").gameObject;
                 obj.SetActive(true);
                 obj.GetComponent<Animator>().SetTrigger("open");
-                scroll.Open(i+1);
+                scroll.Open(i);
+            }
+            else if (data.GetStageStatus(i) == (int)Data.STAGE_STATUS.NONE)
+            {
+                transform.Find(s).Find("lock").gameObject.SetActive(true);
             }
 
+
         }
+
+        if (data.GetStageStatus(0) == (int)Data.STAGE_STATUS.NONE)
+            transform.Find("stage01").Find("lock").gameObject.SetActive(true);
     }
 }
